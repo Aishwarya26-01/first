6d59092a213d974bb565d44a3aa178bf6811ae17
Aishwarya26-01 <aishwaryamishra2019@gmail.com>
2023-03-14 10:07:20 +0530

    dom_task4

--- dom.js
+++ dom.js
@@ -14,3 +14,15 @@ headerTitle.style.border='solid 3px #000';
 var title1=document.getElementsByClassName('title');
 title1[0].style.color='green';
 title1[0].style.fontWeight='bold';
+
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight = 'bold';
+    items[i].style.backgroundColor = '#f4f4f4';
+}
+items[2].style.backgroundColor = 'green';
