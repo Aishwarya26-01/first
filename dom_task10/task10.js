8fb8c826dd95e0908fbc4a47b61545fad4a34c15
Aishwarya26-01 <aishwaryamishra2019@gmail.com>
2023-03-23 14:46:03 +0530

    dom_task10

---
+++ dom_task10/task10.js
@@ -0,0 +1,9 @@
+function saveTOlocalstorage(event){
+    event.preventDefault();
+    const name=event.target.username.value;
+    const email=event.target.emailid.value;
+    const phonenumber=event.target.phonenumber.value;
+    localStorage.setItem('name',name);
+    localStorage.setItem('email',email);
+    localStorage.setItem('phonenumber',phonenumber);
+}
